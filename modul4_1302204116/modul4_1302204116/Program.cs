92a67c5a8276d05f53116d8533f660b7f48bb424
unknown <rafialif2907@gmail.com>
2022-03-19 10:52:54 +0700

    menambahkan class SimpleData

--- modul4_1302204116/modul4_1302204116/Program.cs
+++ modul4_1302204116/modul4_1302204116/Program.cs
@@ -8,8 +8,42 @@ namespace modul4_1302204116
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            SimpleDataBase<int> objData = new SimpleDataBase<int>();
+            objData.AddNewData(13);
+            objData.AddNewData(02);
+            objData.AddNewData(20);
+            objData.PrintAllData();
+        }
+    }
+
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newdata)
+        {
+            storedData.Add(newdata);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " berisi : " + storedData[i] + " yang disimpan pada waktu UTC : " + inputDates[i] + " AM");
+            }
         }
     }
 }
+
+
+
